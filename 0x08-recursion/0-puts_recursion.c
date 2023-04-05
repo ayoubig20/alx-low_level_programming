9c01e256cb2a70be2e34ecb8acef552e858e4e94
ayoub iguich <iguich.ayouub@gmail.com>
2023-04-05 10:58:36 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,28 @@
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_puts_recursion - Prints a string
+ *
+ *@s: char*
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] != '\0')
+	{
+		_putchar(s[0]);
+		if (s[1])
+		{
+			_puts_recursion(&s[1]);
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
