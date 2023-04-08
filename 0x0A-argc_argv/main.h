aa4bf6bb6c18ddcd1040256b2301e6bce1e353bc
ayoub iguich <iguich.ayouub@gmail.com>
2023-04-08 10:57:14 -0700

    task 0

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(char c);
+void print_number(int n);
+int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
+int _convert_to_int(char *s);
+int _abs(int n);
+void print_err(void);
+int _check_min_change(long n);
+#endif
